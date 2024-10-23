b0a36809c5553a19ad1bf200370dbeb187378eb3
Vanshaj Saxena <vs110405@outlook.com>
2024-10-23 18:15:31 +0530

    refactor(): configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,23 +1,16 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { nixos-stable, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
-  # Experimental nix-command and flakes support
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Hibernate
   swapDevices = [
   { device = "/dev/disk/by-uuid/addd79fc-3c3c-4562-9a97-b895abf8945e";
   }
@@ -26,24 +19,12 @@
   boot.resumeDevice = "/dev/disk/by-uuid/addd79fc-3c3c-4562-9a97-b895abf8945e";
   boot.kernelParams = [ "resume=/dev/disk/by-uuid/addd79fc-3c3c-4562-9a97-b895abf8945e" ];
 
-  powerManagement.enable = true;
-  powerManagement.cpuFreqGovernor = "ondemand";
-  # Hibernate settings end
+  networking.hostName = "NIXOS";
 
-  networking.hostName = "NIXOS"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Asia/Kolkata";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -58,27 +39,20 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
 
-  # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  # Configure keymap in X11
   services.xserver = {
     xkb.layout = "us";
     xkb.variant = "";
   };
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
 
-  # Bluetooth support
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
 
@@ -88,32 +62,12 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
-  "monitor.bluez.properties" = {
-      "bluez5.hw-volume" = [ "hfp_hf" "hsp_hs" "a2dp_sink" "hsp_ag" "hfp_ag" "a2dp_source"];
-      "bluez5.enable-hw-volume" = true;
-  };
-};
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.vanshaj = {
     isNormalUser = true;
     description = "Vanshaj Saxena";
     extraGroups = [ "networkmanager" "wheel" ];
-    # packages = with nixos-stable; [
-    # #  thunderbird
-    # ];
     shell = nixos-stable.zsh;
   };
 
@@ -121,24 +75,19 @@ services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
     (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" "VictorMono" ]; })
   ];
 
-  # Steam
-  programs.steam.enable = true;
+  programs.steam.enable = false;
 
-  # Install firefox.
   programs.firefox = {
       enable = true;
-    };
+  };
+
   programs.zsh.enable = true;
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with nixos-stable; [
    git
    vim # Do not forget to add an editor to edit configuration.nix!
-   # icu # unicode and globalization support library
    tmux
    gcc
    wl-clipboard
@@ -147,48 +96,21 @@ services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
    curl
   ];
 
-  # Environment variables
   environment.variables = {
    EDITOR = "nvim";
    PAGER = "bat";
   };
 
-
   programs.nix-ld.enable = true;
-  # programs.nix-ld.libraries = with nixos-stable; [
-  #   # Add any missing dynamic libraries for unpackaged 
-  #   # programs here, NOT in environment.systemPackages
-  # ];
 
-  # Perform garbage collection weekly to maintain low disk usage
   nix.gc = {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 1w";
   };
 
-  # Refer to the following link for more details:
-  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
   nix.settings.auto-optimise-store = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
   networking.firewall = {
     enable = true;
     allowedTCPPortRanges = [
@@ -207,5 +129,4 @@ services.pipewire.wireplumber.extraConfig.bluetoothEnhancements = {
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
 
   system.stateVersion = "24.05"; # Did you read the comment?
-
 }
