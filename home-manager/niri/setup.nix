0e371edf679117e42f8b1ad3383deaf78699df3f
Vanshaj Saxena <vs110405@outlook.com>
2025-11-16 18:47:15 +0530

    fix: use xdg.portal to use portals

--- home-manager/niri/setup.nix
+++ home-manager/niri/setup.nix
@@ -9,11 +9,6 @@
   };
 
   home.packages = with pkgs; [
-    # https://yalter.github.io/niri/Important-Software.html#portals
-    xdg-desktop-portal-gtk
-    xdg-desktop-portal-gnome
-    gnome-keyring
-
     xwayland-satellite
     waybar
     fuzzel
@@ -31,4 +26,16 @@
       enable = true;
     };
   };
+
+  # https://yalter.github.io/niri/Important-Software.html#portals
+  services.gnome-keyring.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+    ];
+  };
+
 }
