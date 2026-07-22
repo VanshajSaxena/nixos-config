fc0e9c164be92c1486511190824fd8f1fa06bb4b
Vanshaj Saxena <vs110405@outlook.com>
2026-07-22 13:24:40 +0530

    feat: create yazi config with home-manager

--- home-manager/programs/tools.nix
+++ home-manager/programs/tools.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   programs.git = {
     enable = true;
@@ -51,6 +51,117 @@
     enable = true;
   };
 
+  programs.yazi = {
+    enable = true;
+    shellWrapperName = "y";
+
+    settings = {
+      mgr = {
+        linemode = "size_and_mtime";
+      };
+    };
+
+    initLua = ''
+      function Linemode:size_and_mtime()
+        local time = math.floor(self._file.cha.mtime or 0)
+
+        if time == 0 then
+          time = ""
+        elseif os.date("%Y", time) == os.date("%Y") then
+          time = os.date("%b %d %H:%M", time)
+        else
+          time = os.date("%b %d  %Y", time)
+        end
+
+        local size = self._file:size()
+
+        return string.format(
+          "%9s %s",
+          size and ya.readable_size(size) or "-",
+          time
+        )
+      end
+    '';
+
+    plugins = {
+      git = {
+        package = pkgs.yaziPlugins.git;
+        setup = true;
+
+        settings = {
+          refresh = false;
+          update_after_op = true;
+          show_ignored = false;
+
+          debounce = 150;
+          max_depth = 8;
+
+          symbols = {
+            added = "A";
+            modified = "M";
+            deleted = "D";
+            renamed = "R";
+            copied = "C";
+            typechanged = "T";
+            unmerged = "U";
+            untracked = "";
+            ignored = "I";
+            external = "";
+            clean = "";
+          };
+
+          colors = {
+            added = "green";
+            modified = "yellow";
+            deleted = "red";
+            renamed = "cyan";
+            copied = "green";
+            typechanged = "magenta";
+            unmerged = "red";
+            untracked = "blue";
+            ignored = "brightblack";
+            external = "cyan";
+          };
+        };
+      };
+
+      full-border = {
+        package = pkgs.yaziPlugins.full-border;
+        setup = true;
+
+        settings = {
+          type = lib.generators.mkLuaInline "ui.Border.ROUNDED";
+        };
+      };
+
+      # Hide the status line entirely.
+      no-status = {
+        package = pkgs.yaziPlugins.no-status;
+        setup = true;
+      };
+
+      # Functional plugins don't require setup().
+      jump-to-char.package = pkgs.yaziPlugins.jump-to-char;
+      smart-enter.package = pkgs.yaziPlugins.smart-enter;
+    };
+
+    keymap = {
+      mgr.prepend_keymap = [
+        {
+          on = "f";
+          run = "plugin jump-to-char";
+          desc = "Jump to file by first character";
+        }
+
+        {
+          on = "l";
+          run = "plugin smart-enter";
+          desc = "Enter directory or open file";
+        }
+      ];
+    };
+  };
+
   home.packages = with pkgs; [
     lazygit
     ripgrep
@@ -58,7 +169,6 @@
     fzf
     tig
     htop
-    yazi
     xdotool # X11 automation tool
     dust # du alternative
     scc # code counter
@@ -71,7 +181,8 @@
     silicon
     kdePackages.kdeconnect-kde # kde-connect
     webcamoid # webcam
-    # ffmpeg_7 # GIF and videos
+    ffmpeg # GIF and videos
+    wtype
   ];
 
 }
