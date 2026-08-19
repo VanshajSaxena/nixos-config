6550a0b3619df2ec15dc8c87735900a2cd4d9f41
Vanshaj Saxena <vs110405@outlook.com>
2026-08-19 22:51:51 +0530

    feat: switch to `ly` display manager

--- configuration.nix
+++ configuration.nix
@@ -85,7 +85,7 @@
 
   services.xserver.enable = true;
 
-  services.displayManager.sddm.enable = true;
+  services.displayManager.ly.enable = true;
   services.desktopManager.plasma6.enable = true;
   environment.plasma6.excludePackages = [ nixos-stable.kdePackages.kate ];
 
